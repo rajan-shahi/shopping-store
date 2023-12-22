625b93ca9cdba2e8dbe99c220d929d1ac0f0c304
rajan <rajanshahi1267@gmail.com>
2023-12-21 22:50:49 -0700

    ad

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import rajan from "../assets/rajan.jpg";
 import { Link } from "react-router-dom";
 
 const Profile = () => {
+  const [edit, setEdit] = useState(true);
+  const openclose = () => {
+    setEdit(!edit);
+  };
   return (
     <div className=" flex justify-center">
       <div className=" flex  md:pt-24 pt-16 md:pb-10 flex-col gap-20 w-full md:px-0 px-4 md:w-10/12">
@@ -18,11 +22,16 @@ const Profile = () => {
               Hello , Rajan Bahadur Shahi.
             </h1>
             <div className=" flex gap-5">
-
-              <Link to={"/buy"} className=" hover:scale-105 duration-500 bg-black text-white px-6 py-1">
+              <Link
+                to={"/buy"}
+                className=" hover:scale-105 duration-500 bg-black text-white px-6 py-1"
+              >
                 LOGOUT
               </Link>
-              <button className=" hover:scale-105 duration-500 bg-black text-white px-6 py-1">
+              <button
+                onClick={openclose}
+                className=" relative  hover:scale-105 duration-500 bg-black text-white px-6 py-1"
+              >
                 EDIT PROFILE
               </button>{" "}
             </div>
@@ -48,8 +57,53 @@ const Profile = () => {
             <h2 className=" text-md">9866448606</h2>
           </div>
         </div>
+        {/* profile edit */}
+        {!edit && (
+          <div className="md:top-6 top-7 right-1  md:right-72 absolute bg-white z-20  border border-gray-300 py-8 px-5 rounded-2xl md:w-4/12 w-full">
+            <div className=" flex flex-col gap-5">
+              <h1 className=" flex justify-center text-xl text-gray-600">
+                UPDATE YOUR INFORMATION
+              </h1>
+              <input
+                className=" border border-gray-300 py-2 placeholder:px-2 placeholder:text-gray-700"
+                type="text"
+                placeholder="Full Name"
+              />
+              <input
+                className=" border border-gray-300 py-2 placeholder:px-2 placeholder:text-gray-700"
+                type="email"
+                name="email"
+                placeholder="Email"
+              />
+              <input
+                className=" border border-gray-300 py-2 placeholder:px-2 placeholder:text-gray-700"
+                type="number"
+                placeholder="Phone Number"
+              />
+
+              <h1>Profile Picture</h1>
+              <input type="file" />
+
+              <div className=" flex gap-3">
+                <Link
+                  onClick={openclose}
+                  to={"/profile"}
+                  className=" flex justify-center border-2 border-black w-full bg-gray-100 py-1"
+                >
+                  Close
+                </Link>
+                <button className=" w-full bg-black text-white py-1">
+                  UPDATE
+                </button>
+              </div>
+              
+            </div>
+          </div>
+        )}
+        
       </div>
     </div>
+    
   );
 };
 
